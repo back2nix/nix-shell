d2886bd70ae58a86df6ec62b3f526972930babde
Back Nix <back2nix@list.ru>
2023-09-13 00:58:04 +0300

    gaussian

--- python/nerfstudio/shell.nix
+++ python/nerfstudio/shell.nix
@@ -11,61 +11,84 @@
 #   )
 # , # , cachnix ? import <nixpkgs> { }
 # }:
-{ pkgs ? import <nixpkgs> {
-    config = {
-      allowUnfree = true;
-      cudaSupport = true;
-    };
-  }
-,
-}:
-let
-  diff-gaussian-rasterization = pkgs.python3Packages.callPackage ./third-party/diff-gaussian-rasterization/default.nix { };
-  simple-knn = pkgs.python3Packages.callPackage ./third-party/diff-gaussian-rasterization/default.nix { };
-in
-pkgs.mkShell
 {
-  name = "diff-gaussian-shell";
-  # https://www.youtube.com/watch?v=A1Gbycj0bWw&ab_channel=TheNeRFGuru
+  pkgs ?
+    import <nixpkgs> {
+      config = {
+        allowUnfree = true;
+        cudaSupport = true;
+      };
+    },
+}: let
+  # diff-gaussian-rasterization = pkgs.python3Packages.callPackage ./third-party/diff-gaussian-rasterization/default.nix { };
+  # simple-knn = pkgs.python3Packages.callPackage ./third-party/diff-gaussian-rasterization/default.nix { };
+  stdenvGCC11 = pkgs.overrideCC pkgs.stdenv pkgs.gcc11;
+  cudatoolkit = pkgs.cudaPackages_11_7.cudatoolkit.override {
+    stdenv = stdenvGCC11;
+  };
+in
+  pkgs.mkShell.override
+  {
+    stdenv = pkgs.gcc11Stdenv;
+  }
+  # pkgs.mkShell
+  {
+    name = "diff-gaussian-shell";
+    # https://www.youtube.com/watch?v=A1Gbycj0bWw&ab_channel=TheNeRFGuru
 
-  buildInputs = with pkgs.python310Packages; [
-    pkgs.python310
-    pkgs.cudaPackages.cudatoolkit
-    diff-gaussian-rasterization
-    fastai
-    fastdownload
-    pytorch-bin
-    unidecode
-    # torchaudio
-    # inflect
-    # cachnix.python310Packages.accelerate
-    librosa
-    pip
-    pandas
-    pillow
-    scipy
-    requests
-    scikit-learn
-    matplotlib
-    pyyaml
-    accelerate
-    ipywidgets
-    plyfile
-    tqdm
-    diff-gaussian-rasterization
-    simple-knn
-    venvShellHook
-  ];
-  venvDir = "venv310";
+    buildInputs = with pkgs.python310Packages; [
+      # pkgs.gcc11
+      # pkgs.gcc11
+      # pkgs.gcc11Stdenv.glibc
 
-  doCheck = false;
+      pkgs.python310
+      pkgs.glm
+      cudatoolkit
+      # diff-gaussian-rasterization
+      # simple-knn
+      fastai
+      fastdownload
+      pytorch-bin
+      unidecode
+      # torchaudio
+      # inflect
+      # cachnix.python310Packages.accelerate
+      librosa
+      pip
+      pandas
+      pillow
+      scipy
+      requests
+      scikit-learn
+      matplotlib
+      pyyaml
+      accelerate
+      ipywidgets
+      plyfile
+      tqdm
+      venvShellHook
+    ];
+    venvDir = "venv310";
 
-  shellHook = ''
-    echo "You are now using a NIX environment"
-    export CUDA_PATH=${pkgs.cudatoolkit}
-  '';
+    doCheck = false;
 
-  # env = {
-  #   CUDA_PATH = ${pkgs.cudatoolkit};
-  # };
-}
+    postShellHook = ''
+      pip install submodules/diff-gaussian-rasterization
+      pip install submodules/simple-knn
+
+      echo "You are now using a NIX environment"
+      export CUDA_PATH=${cudatoolkit}
+    '';
+
+    # shellHook = ''
+    #   pip install submodules\diff-gaussian-rasterization
+    #   pip install submodules\simple-knn
+    #
+    #   echo "You are now using a NIX environment"
+    #   export CUDA_PATH=${pkgs.cudatoolkit}
+    # '';
+
+    # env = {
+    #   CUDA_PATH = ${pkgs.cudatoolkit};
+    # };
+  }
