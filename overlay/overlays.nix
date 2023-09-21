874a9a9fdf24c3cc15058abfef538e7d782db96c
back2nix <back2nix@list.ru>
2023-09-21 21:21:13 +0300

    save

--- overlay/overlays.nix
+++ overlay/overlays.nix
@@ -1,20 +1,15 @@
 self: super: {
   go_1_21 = self.darwin.apple_sdk_11_0.callPackage ./go/1.21.nix {
-    inherit (self.darwin.apple_sdk_11_0.frameworks) Foundation Security;
+    inherit (self.darwin.apple_sdk_11_0.frameworks) Foundation Security buildGo121Package;
   };
 
   buildGo121Module = self.darwin.apple_sdk_11_0.callPackage ../go/module.nix {
-    go = self.darwin.apple_sdk_11_0.callPackage ./go/1.21.nix {
-      inherit (self.darwin.apple_sdk_11_0.frameworks) Foundation Security;
-    };
-  };
-  buildGo121Package = self.darwin.apple_sdk_11_0.callPackage ../go/package.nix {
-    go = self.darwin.apple_sdk_11_0.callPackage ./go/1.21.nix {
-      inherit (self.darwin.apple_sdk_11_0.frameworks) Foundation Security;
-    };
+    go = self.go_1_21;
   };
 
-  go = self.darwin.apple_sdk_11_0.callPackage ./go/1.21.nix {
-    inherit (self.darwin.apple_sdk_11_0.frameworks) Foundation Security buildGo121Package;
+  buildGo121Package = self.darwin.apple_sdk_11_0.callPackage ../go/package.nix {
+    go = self.go_1_21;
   };
+
+  go = self.go_1_21;
 }
