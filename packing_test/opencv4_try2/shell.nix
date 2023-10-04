e25ca950fc0a93287352b07f537fee5cc8499ce0
back2nix <back2nix@list.ru>
2023-10-05 00:52:57 +0300

    improve opencv shell

--- packing_test/opencv4_try2/shell.nix
+++ packing_test/opencv4_try2/shell.nix
@@ -1,8 +1,7 @@
-{ pkgs ? import <nixpkgs> { } }:
-let
+{pkgs ? import <nixpkgs> {}}: let
   opencv = pkgs.python310Packages.opencv4.overrideAttrs (finalAttrs: old: {
-    # dontStrip = true;
-    # cmakeBuildType = "Debug"; # if your wants debug build
+    dontStrip = true;
+    cmakeBuildType = "Debug"; # if your wants debug build
 
     # sourceRoot = ".";
 
@@ -23,33 +22,27 @@ let
       echo Path:
       echo $out
 
-      # for get prebuild + source folders
-      # mkdir -p $out/build
-      # cp -r * $out/build
-      # false
+      ## -- for get prebuild + source folders
+      mkdir -p $out/build
+      cp -r * $out/build
+      false
     '';
 
     preInstall = ''
     '';
 
-    # git clone --branch 4.7.0 https://github.com/opencv/opencv.git
-    # git clone --branch 4.7.0 https://github.com/opencv/opencv_contrib.git
-    # git clone --branch 4.7.0 https://github.com/opencv/opencv_extra.git
-
-    src = /home/bg/Documents/code/github.com/back2nix/nix/nix-shell/packing_test/opencv4_try2/release/source;
-    # src = builtins.fetchGit /home/bg/Documents/code/github.com/back2nix/nix/nix-shell/packing_test/opencv/source/opencv;
-    # contribSrc = builtins.fetchGit /home/bg/Documents/code/github.com/back2nix/nix/nix-shell/packing_test/opencv/source/opencv_contrib;
-    # testDataSrc = builtins.fetchGit /home/bg/Documents/code/github.com/back2nix/nix/nix-shell/packing_test/opencv/source/opencv_extra;
+    src = ./debug/source;
+    # src = builtins.fetchGit /home/bg/debug/source; # without prebuild, only source
   });
 in
-pkgs.mkShell
-{
-  name = "custom-opencv4-shell";
+  pkgs.mkShell
+  {
+    name = "custom-opencv4-shell";
 
-  buildInputs = with pkgs; [
-    stdenv
-    gdb
-    cmake
-    opencv
-  ];
-}
+    buildInputs = with pkgs; [
+      stdenv
+      gdb
+      cmake
+      opencv
+    ];
+  }
